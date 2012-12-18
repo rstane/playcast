ced5ea56e5afe0668c00de93e03b67f8be9b6fce
shu0115 <s.matsumoto0115@gmail.com>
2012-12-18 17:42:21 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playcast::Application.config.secret_token = 'ae023a9f467c3041204bd37cdc407e16ba692a78e323149c2dfbf222b082f9a52fbd2e66b3db36cd1fb48f11654f7cc6a9f663e634e84b114857e62ff2dc65e3'
