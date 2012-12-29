5c6dab26c68b2b2e224bde1404efeb103a87e5d8
shu0115 <s.matsumoto0115@gmail.com>
2012-12-29 14:31:11 +0900

    不要ファイル削除

--- config/initializers/exception_notification.rb
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-Rails.application.config.middleware.use(
-  ExceptionNotifier,
-  email_prefix:         "[#{Rails.env}][#{Settings.app_name}] ",
-  sender_address:       "rails.dev0115@gmail.com",
-  exception_recipients: [ "rails.dev0115@gmail.com" ]
-)
