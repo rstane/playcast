d4cfc6ed532558d4730f1d3dd013bed8c777704a
shu0115 <s.matsumoto0115@gmail.com>
2013-02-11 19:39:31 +0900

    プランタイトル文字数制限変更

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -17,7 +17,7 @@ class Plan < ActiveRecord::Base
   after_create :create_feed_plan_start
 
   # バリデーション
-  validates :title, presence: true, length: { maximum: 300 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :place, presence: true, length: { maximum: 500 }
   validates :budget, presence: true, length: { maximum: 100 }
