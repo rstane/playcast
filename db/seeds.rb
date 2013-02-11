92cfe1ea61c82361e573205a4aa787ab868832d6
shu0115 <s.matsumoto0115@gmail.com>
2013-02-11 17:59:36 +0900

    カテゴリ全削除コメントアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # カテゴリ
-Category.delete_all
+#Category.delete_all
 Category.where( name: "カラオケ・アミューズメント", sort: 1 ).first_or_create
 Category.where( name: "飲み会・食事会", sort: 2 ).first_or_create
 Category.where( name: "スポーツ・アウトドア", sort: 3 ).first_or_create
