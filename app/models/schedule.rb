90e1d3fa1af4a371ab56c0ca75f3809a05a017cc
shu0115 <s.matsumoto0115@gmail.com>
2013-02-16 16:40:39 +0900

    issue #39

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,19 @@ class Schedule < ActiveRecord::Base
 
   belongs_to :plan
   has_many :participations
+
+  # コールバック
+  before_save :setting_close_at
+
+  private
+
+  #------------------#
+  # setting_close_at #
+  #------------------#
+  # 募集終了時刻格納
+  def setting_close_at
+    if self.candidate_day.present?
+      self.close_at = self.candidate_day.end_of_day
+    end
+  end
 end
