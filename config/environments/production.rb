cbb350fab9858cd8e26abcf27f403233bb39b616
shu0115 <s.matsumoto0115@gmail.com>
2013-02-28 16:14:02 +0900

    メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Playcast::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    address:              "smtp.gmail.com",
+    address:              "smtp.live.com",
     port:                 587,
-    user_name:            "rails.dev0115@gmail.com",
+    user_name:            "info@playcast.jp",
     password:             Settings.mail_password,
     authentication:       'plain',
     enable_starttls_auto: true,
