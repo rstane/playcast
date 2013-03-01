1b67fdc79c622c865499b9ce09504c9f834bf742
shu0115 <s.matsumoto0115@gmail.com>
2013-03-01 15:51:05 +0900

    Boardバリデーション追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,10 @@
 class Board < ActiveRecord::Base
   attr_accessible :plan_id
 
+  # アソシエーション
   belongs_to :plan
   has_many :board_comments
+
+  # バリデーション
+  validates :plan_id, presence: true
 end
