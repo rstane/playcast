db1ef8e648b49bbc1bb53b1ef8f60246bcee6bbf
shu0115 <s.matsumoto0115@gmail.com>
2013-03-01 20:28:02 +0900

    user_id取得方法修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,6 +61,5 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.where( id: session[:user_id] ).first
   end
-
   helper_method :current_user
 end
