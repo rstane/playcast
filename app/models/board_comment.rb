b21141636194253f76d12cf44b1def392732db6a
shu0115 <s.matsumoto0115@gmail.com>
2013-03-14 18:26:53 +0900

    待ち合わせ相談ルームコメント即時メール送信

--- app/models/board_comment.rb
+++ app/models/board_comment.rb
@@ -16,7 +16,18 @@ class BoardComment < Comment
     schedules = Schedule.where( plan_id: plan.id, adopt_flag: true ).includes( :participations )
     schedules.each { |s|
       s.participations.each { |participation|
-        FeedComment.where( comment_id: self.id, plan_id: self.plan_id, user_id: participation.user_id, happen: "待ち合わせ相談ルームにコメントが投稿されました。", send_mail_flag: true ).first_or_create
+        # コメント投稿者で無ければ
+        unless self.user_id == participation.user_id
+          # フィード作成
+          feed = FeedComment.where( comment_id: self.id, plan_id: self.plan_id, user_id: participation.user_id, happen: "待ち合わせ相談ルームにコメントが投稿されました。", send_mail_flag: false ).first_or_initialize
+
+          if feed.id.blank?
+            if feed.save
+              # メール送信
+              UserMailer.plan_feed( feed.plan, feed, feed.user ).deliver
+            end
+          end
+        end
       }
     }
   end
