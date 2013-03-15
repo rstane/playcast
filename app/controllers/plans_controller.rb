bd0b43999e7602e45b7444ee636b61c1bbb992c8
shu0115 <s.matsumoto0115@gmail.com>
2013-03-15 17:35:47 +0900

    性別ごとプラン詳細閲覧判定

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -47,6 +47,16 @@ class PlansController < ApplicationController
       redirect_to plans_path and return
     end
 
+    # 性別ごと募集終了判定
+    if @plan.gender_entry_close?( current_user.try(:gender) )
+      # 参加者チェック
+      unless @plan.participant?( session[:user_id] )
+        flash[:alert] = "募集終了後のプランは参加者以外閲覧出来ません。<br>新しく遊ぶメンバーを募集してみましょう↓"
+        redirect_to plans_path and return
+      end
+    end
+
+    # 募集終了判定
     if @plan.closed?
       # 参加者チェック
       unless @plan.participant?( session[:user_id] )
